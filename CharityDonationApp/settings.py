503b2d9f986fefa8c235fdc53f69c5477561804e
Filip Barański <filip.baaranski@gmail.com>
2024-07-15 12:47:00 +0200

    static files loading fix

--- CharityDonationApp/settings.py
+++ CharityDonationApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CharityDonationApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'charity/templates']
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'charity/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
